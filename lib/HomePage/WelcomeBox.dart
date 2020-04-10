098cdd54885978a1b25e138c91a3dc95aae9fbcd
Kirill <kirillmasychev@outlook.com>
2020-04-10 19:54:39 -0400

    added instructions to welcome page

--- lib/HomePage/WelcomeBox.dart
+++ lib/HomePage/WelcomeBox.dart
@@ -31,9 +31,22 @@ class WelcomeBox extends StatelessWidget{
                   child: Text("  How to use this site: ", style: Theme.of(context).textTheme.display1, textScaleFactor: .7, /*TextStyle(fontSize: 24)*/),
                 ),
                 Divider(color: Colors.white),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text("  1. Find the item on Superdry.com  (US site only at the moment) \n  2. Copy the link of the item \n  3. Paste the link in the search bar above and hit enter", style: Theme.of(context).textTheme.display1, textScaleFactor: .55 /*TextStyle(fontSize: 18)*/,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("  1. Find the item on Superdry.com  (US site) \n  2. Copy the URL of the item  \n  3. Paste the link in the search bar above \n      and hit enter", style: Theme.of(context).textTheme.display1, textScaleFactor: .55 /*TextStyle(fontSize: 18)*/,),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(" OR ", style: Theme.of(context).textTheme.display1,),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("  1. While Browsing www.superdry.com \n  2. Change the domain of the URL   \n  3. from: www.superdry.com/us/mens/jackets/details/.../ \n  4. to: www.colliecolliecollie.com/us/mens/jackets/details/.../ ", style: Theme.of(context).textTheme.display1, textScaleFactor: .50 /*TextStyle(fontSize: 18)*/,),
+                    ),
+                  ],
                 ),
                 Divider(color: Colors.white)],
             ),
